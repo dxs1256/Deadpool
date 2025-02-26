15c324a26c9847a3e0ae44fcec81beac6b377b94
dxs1256 <23136671+dxs1256@users.noreply.github.com>
2025-02-27 01:24:51 +0800

    Update main.py

--- main.py
+++ main.py
@@ -124,8 +124,6 @@ def write_proxies_to_file(filename: str, proxies: List[str]):
     """写入文件"""
     try:
         with open(filename, "w") as f:
-            f.write(f"# Updated at: {time.strftime('%Y-%m-%d %H:%M:%S UTC')}\n")
-            f.write(f"# Random ID: {uuid.uuid4()}\n")  # 确保每次写入都不同
             for proxy in proxies:
                 f.write(proxy + "\n")
         logging.info(f"已将 {len(proxies)} 个代理写入 {filename}")
